e36c6105d15b5e3e8527d49f690d1c3bca78f1fd
Carlie Duperval <ritchellcarlie@gmail.com>
2022-07-01 08:32:27 -0400

    Commit added

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-const uds = 3;
-const haitianGourde = 108;
+// let celcius = null;
 
-function convertUstoGourde (usdConverter, rateConverter){
-    const converted = usdConverter * rateConverter
-    return converted;
+// function findTemperatureFar (Ftemp){
+//     const fahrenheit = (Ftemp * 9/5) + 32
+//     return fahrenheit
 
+// }
+
+// function findKtemperature(Ktemp){
+//     const kelvin = (Ktemp+ 274.15)
+//     return kelvin
+
+// }
+// const tempFar =findTemperatureFar(celcius = 4)
+// const tempK = findKtemperature(celcius =6)
+// console.log('This is ',tempFar, 'in farhenheit')
+// console.log('This is ',tempK, 'in kelvin')
+// console.log(celcius, 'is', celcius, 'celcius')
+
+
+//let celcius1 = 0;
+
+function tempConverter(temp){
+    const fahrenheit = (temp * 9/5) + 32
+    const kelvin = (temp+ 274.15)
+    const tempObject = {
+        celcius : temp,
+        fahrenheit : fahrenheit,
+        kelvin : kelvin
+    }
+    return tempObject
 }
 
-const newValue = convertUstoGourde(uds, haitianGourde)
-console.log("There are " , uds, "UsD in", newValue + " Gourdes")
+console.log(tempConverter(45).fahrenheit)
+console.log(tempConverter(12).kelvin)
+console.log(tempConverter(10).celcius)
+
+
